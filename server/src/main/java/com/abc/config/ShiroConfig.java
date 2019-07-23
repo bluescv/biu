93e3f94c0278eb5f9d5cf7edf06d4655cc7a3b19
bluescv <bluescv@gmail.com>
2019-07-23 17:17:30 +0800

    add shiro config and custom realm and gradle

--- server/src/main/java/com/abc/config/ShiroConfig.java
+++ server/src/main/java/com/abc/config/ShiroConfig.java
@@ -1,19 +1,72 @@
 package com.abc.config;
 
-import com.abc.shiro.UserRealm;
-import org.apache.shiro.realm.Realm;
+import com.abc.shiro.MyShiroCasRealm;
+import org.apache.shiro.cas.CasFilter;
+import org.apache.shiro.cas.CasSubjectFactory;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import javax.servlet.Filter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 public class ShiroConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ShiroConfig.class);
+
+    // CasServerUrlPrefix
+    public static final String casServerUrlPrefix = "http://localhost:8085/";
+    // Cas登录页面地址
+    public static final String casLoginUrl = casServerUrlPrefix + "/login";
+    // Cas登出页面地址
+    public static final String casLogoutUrl = casServerUrlPrefix + "/logout";
+    // 当前工程对外提供的服务地址
+    public static final String shiroServerUrlPrefix = "http://localhost:8888";
+    // casFilter UrlPattern
+    public static final String casFilterUrlPattern = "/shiro-cas";
+    // 登录地址
+    public static final String loginUrl = casLoginUrl + "?service=" + shiroServerUrlPrefix;
+
+    @Bean(name = "myShiroCasRealm")
+    public MyShiroCasRealm myShiroCasRealm() {
+        MyShiroCasRealm realm = new MyShiroCasRealm();
+        return realm;
+    }
 
     @Bean
-    public Realm realm() {
-        return new UserRealm();
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
+        // 该值缺省为false,表示生命周期由SpringApplicationContext管理,设置为true则表示由ServletContainer管理
+        filterRegistration.addInitParameter("targetFilterLifecycle", "true");
+        filterRegistration.setEnabled(true);
+        filterRegistration.addUrlPatterns("/*");
+        return filterRegistration;
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor getAuthorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor aasa = new AuthorizationAttributeSourceAdvisor();
+        aasa.setSecurityManager(securityManager);
+        return aasa;
     }
 
     @Bean
@@ -28,27 +81,83 @@ public class ShiroConfig {
         return creator;
     }
 
+    @Bean(name = "casFilter")
+    public CasFilter getCasFilter() {
+        CasFilter casFilter = new CasFilter();
+        casFilter.setName("casFilter");
+        casFilter.setEnabled(true);
+        // 登录失败后跳转的URL，也就是 Shiro 执行 CasRealm 的 doGetAuthenticationInfo 方法向CasServer验证tiket
+        casFilter.setFailureUrl(loginUrl);// 我们选择认证失败后再打开登录页面
+        return casFilter;
+    }
+
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(MyShiroCasRealm myShiroCasRealm) {
+        DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
+        dwsm.setRealm(myShiroCasRealm);
+//   <!-- 用户授权/认证信息Cache, 采用EhCache 缓存 -->
+        // 指定 SubjectFactory
+        dwsm.setSubjectFactory(new CasSubjectFactory());
+        return dwsm;
+    }
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager securityManager, CasFilter casFilter) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        // 必须设置 SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        shiroFilterFactoryBean.setLoginUrl(loginUrl);
+        // 登录成功后要跳转的连接
+        shiroFilterFactoryBean.setSuccessUrl("/login");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        // 添加casFilter到shiroFilter中
+        Map<String, Filter> filters = new HashMap<>();
+        filters.put("casFilter", casFilter);
+        shiroFilterFactoryBean.setFilters(filters);
+
+
+        /////////////////////// 下面这些规则配置最好配置到配置文件中 ///////////////////////
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+
+        filterChainDefinitionMap.put(casFilterUrlPattern, "casFilter");// shiro集成cas后，首先添加该规则
+
+        // authc：该过滤器下的页面必须验证后才能访问，它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter
+        filterChainDefinitionMap.put("/auth/login", "authc");// 这里为了测试，只限制/user，实际开发中请修改为具体拦截的请求规则
+        // anon：它对应的过滤器里面是空的,什么都没做
+        logger.info("##################从数据库读取权限规则，加载到shiroFilter中##################");
+        filterChainDefinitionMap.put("/user/edit/**", "authc,perms[user:edit]");// 这里为了测试，固定写死的值，也可以从数据库或其他配置中读取
+
+        filterChainDefinitionMap.put("/login", "authc");
+        filterChainDefinitionMap.put("/**", "authc");//anon 可以理解为不拦截
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+
+        return shiroFilterFactoryBean;
+    }
+
+
     /**
      * 这里统一做鉴权，即判断哪些请求路径需要用户登录，哪些请求路径不需要用户登录。
      * 这里只做鉴权，不做权限控制，因为权限用注解来做。
+     *
      * @return
      */
     @Bean
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
         DefaultShiroFilterChainDefinition chain = new DefaultShiroFilterChainDefinition();
         //哪些请求可以匿名访问
-        chain.addPathDefinition("/auth/login", "anon");
-        chain.addPathDefinition("/auth/logout", "anon");
-        chain.addPathDefinition("/page/401", "anon");
-        chain.addPathDefinition("/page/403", "anon");
-        chain.addPathDefinition("/page/index", "anon");
+//        chain.addPathDefinition("/auth/login", "anon");
+//        chain.addPathDefinition("/auth/logout", "anon");
+//        chain.addPathDefinition("/page/401", "anon");
+//        chain.addPathDefinition("/page/403", "anon");
+//        chain.addPathDefinition("/page/index", "anon");
 
         //除了以上的请求外，其它请求都需要登录
         chain.addPathDefinition("/**", "authc");
         return chain;
     }
-
-
 
 
 }
--- server/src/main/java/com/abc/shiro/MyShiroCasRealm.java
+++ server/src/main/java/com/abc/shiro/MyShiroCasRealm.java
@@ -1,6 +1,7 @@
 package com.abc.shiro;
 
 
+import com.abc.config.ShiroConfig;
 import com.abc.entity.SysUser;
 import com.abc.service.SysPermService;
 import com.abc.service.SysRoleService;
@@ -8,28 +9,26 @@ import com.abc.service.SysUserService;
 import com.abc.vo.AuthVo;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import org.apache.shiro.authc.*;
-import org.apache.shiro.authc.credential.CredentialsMatcher;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.cas.CasRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * 这个类是参照JDBCRealm写的，主要是自定义了如何查询用户信息，如何查询用户的角色和权限，如何校验密码等逻辑
  */
-public class UserRealm extends AuthorizingRealm {
+public class MyShiroCasRealm extends CasRealm {
 
-    private static final Logger log =LoggerFactory.getLogger(UserRealm.class);
+    private static final Logger log = LoggerFactory.getLogger(MyShiroCasRealm.class);
 
     @Autowired
     private SysUserService userService;
@@ -38,15 +37,23 @@ public class UserRealm extends AuthorizingRealm {
     @Autowired
     private SysPermService permService;
 
-    @Override
-    public void setCredentialsMatcher(CredentialsMatcher credentialsMatcher) {
-        //设置用于匹配密码的CredentialsMatcher
-        HashedCredentialsMatcher hashMatcher = new HashedCredentialsMatcher();
-        hashMatcher.setHashAlgorithmName(Sha256Hash.ALGORITHM_NAME);
-        hashMatcher.setStoredCredentialsHexEncoded(false);
-        hashMatcher.setHashIterations(1024);
-        super.setCredentialsMatcher(hashMatcher);
+
+    @PostConstruct
+    public void initProperty() {
+//   setDefaultRoles("ROLE_USER");
+        setCasServerUrlPrefix(ShiroConfig.casServerUrlPrefix);
+        // 客户端回调地址
+        setCasService(ShiroConfig.shiroServerUrlPrefix + ShiroConfig.casFilterUrlPattern);
     }
+//    @Override
+//    public void setCredentialsMatcher(CredentialsMatcher credentialsMatcher) {
+//        //设置用于匹配密码的CredentialsMatcher
+//        HashedCredentialsMatcher hashMatcher = new HashedCredentialsMatcher();
+//        hashMatcher.setHashAlgorithmName(Sha256Hash.ALGORITHM_NAME);
+//        hashMatcher.setStoredCredentialsHexEncoded(false);
+//        hashMatcher.setHashIterations(1024);
+//        super.setCredentialsMatcher(hashMatcher);
+//    }
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
@@ -58,7 +65,7 @@ public class UserRealm extends AuthorizingRealm {
         SysUser user = (SysUser) getAvailablePrincipal(principals);
         Set<AuthVo> roles = user.getRoles();
         Set<AuthVo> perms = user.getPerms();
-        log.info("获取角色权限信息: roles: {}, perms: {}",roles,perms);
+        log.info("获取角色权限信息: roles: {}, perms: {}", roles, perms);
 
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
         info.setRoles(roles.stream().map(AuthVo::getVal).collect(Collectors.toSet()));
@@ -68,9 +75,12 @@ public class UserRealm extends AuthorizingRealm {
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        AuthenticationInfo authc = super.doGetAuthenticationInfo(token);
 
-        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
-        String username = upToken.getUsername();
+        String username = (String) authc.getPrincipals().getPrimaryPrincipal();
+
+//        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+//        String username = upToken.getUsername();
 
         if (username == null) {
             throw new AccountException("用户名不能为空");
@@ -78,7 +88,7 @@ public class UserRealm extends AuthorizingRealm {
 
         SysUser userDB = userService.selectOne(new EntityWrapper<SysUser>().eq("uname", username));
         if (userDB == null) {
-            throw new UnknownAccountException("找不到用户（"+username+"）的帐号信息");
+            throw new UnknownAccountException("找不到用户（" + username + "）的帐号信息");
         }
 
         //查询用户的角色和权限存到SimpleAuthenticationInfo中，这样在其它地方
@@ -97,8 +107,7 @@ public class UserRealm extends AuthorizingRealm {
 
     }
 
-
-
-
-
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
 }
